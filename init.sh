7727e891cfec1c9d57d716931b5a07482eefd2cd
xxai.art <xxai.art@gmail.com>
2023-06-05 14:51:45 +0800

    ♨

--- init.sh
+++ init.sh
@@ -4,21 +4,25 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+direnv allow
+
 rustc -vV | grep "host:"
 
-mkdir -p lib/so
-cd lib/so
-
 version=1.14.1
-name=onnxruntime-osx-arm64-$version
+if [ ! -f "lib/so/onnxruntime.$version.dll" ]; then
+  ort=onnxruntime-osx-arm64-$version
+  mkdir -p lib/so
+  cd lib/so
 
-tgz=$name.tgz
-wget -c https://github.com/microsoft/onnxruntime/releases/download/v$version/$tgz
+  tgz=$ort.tgz
+  wget -c https://github.com/microsoft/onnxruntime/releases/download/v$version/$tgz
 
-rm -rf onnxruntime $name
-tar zxvf $tgz
-ln -s $name onnxruntime
-rm -rf $tgz
-cd onnxruntime/lib
+  tar zxvf $tgz
+  mv $ort/lib/libonnxruntime.$version.dylib $DIR/lib/so/onnxruntime.$version.dll
+  rm -rf $tgz $ort
+fi
 
-ln -s libonnxruntime.dylib libonnxruntime.dll
+cd $DIR/lib
+rm -rf onnxruntime.dll
+ln -s lib/onnxruntime.$version.dll lib/onnxruntime.dll
+cd $DIR
