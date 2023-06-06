1ad3b5a36e53a301be79ff3b3856494cee5fee89
xxai.art <xxai.art@gmail.com>
2023-06-06 14:10:44 +0800

    ♨

--- rust/clip_srv/src/main.rs
+++ rust/clip_srv/src/main.rs
@@ -4,9 +4,9 @@ use std::env::var;
 #[tokio::main]
 async fn main() -> Result<()> {
   let model_dir = var("MODEL_DIR")?;
-  let grpc_port = var("QDRANT__SERVICE__GRPC_PORT")?;
+  let grpc = var("QDRANT_GRPC")?;
   // let config = QdrantClientConfig::from_url("http://localhost:");
 
-  dbg!(model_dir, grpc_port);
+  dbg!(model_dir, grpc);
   Ok(())
 }
