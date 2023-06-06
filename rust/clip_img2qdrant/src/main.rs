680483b6129973a899a5b98b2cf4154ee0de9e53
xxai.art <xxai.art@gmail.com>
2023-06-06 22:18:54 +0800

    ♨

--- rust/clip_img2qdrant/src/main.rs
+++ rust/clip_img2qdrant/src/main.rs
@@ -1,6 +1,34 @@
 mod env;
 // mod img;
 
+use img2qdrant::{
+  img_qdrant_server::{ImgQdrant, ImgQdrantServer},
+  AddIn, AddOut,
+};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod img2qdrant {
+  tonic::include_proto!("img2qdrant");
+}
+
+#[derive(Debug, Default)]
+pub struct _ImgQdrant {}
+
+#[tonic::async_trait]
+impl ImgQdrant for _ImgQdrant {
+  async fn add(
+    &self,
+    request: Request<AddIn>, // 接收以HelloRequest为类型的请求
+  ) -> Result<Response<AddOut>, Status> {
+    // 返回以HelloReply为类型的示例作为响应
+    println!("Got a request: {:?}", request);
+
+    let reply = AddOut {};
+
+    Ok(Response::new(reply)) // 发回格式化的问候语
+  }
+}
+
 use std::sync::OnceLock;
 
 use anyhow::Result;
