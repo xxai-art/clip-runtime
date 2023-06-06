2b9f6dcf6b47a71e1c19459d8cd6c79fa731e0ac
xxai.art <xxai.art@gmail.com>
2023-06-06 16:54:06 +0800

    ♨

--- rust/clip_img2qdrant/src/main.rs
+++ rust/clip_img2qdrant/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use anyhow::Result;
+use clip_qdrant::qdrant_client;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let client = qdrant_client().await?;
+
   println!("Hello, world!");
+  Ok(())
 }
