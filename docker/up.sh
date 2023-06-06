d3e6cec30c42da88e143e288e1cdacad9060d305
xxai.art <xxai.art@gmail.com>
2023-06-06 10:55:11 +0800

    ♨

--- docker/up.sh
+++ docker/up.sh
@@ -4,6 +4,10 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+if [ ! -f "./.env" ]; then
+  cp .env.example .env
+fi
+
 if [ ! -f "./qdrant/config.yml" ]; then
   cp qdrant/config.example.yml qdrant/config.yml
 fi
