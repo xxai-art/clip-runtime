e0df38a1f3a4d6621661c48d8433931fae2eccbb
xxai.art <xxai.art@gmail.com>
2023-06-06 13:52:14 +0800

    ♨

---
+++ rust/clip_srv/dev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+export RUSTFLAGS='--cfg reqwest_unstable'
+export RUST_LOG=$RUST_LOG,watchexec=off,watchexec_cli=off,globset=warn
+
+exec watchexec \
+  --shell=none \
+  --project-origin . \
+  -w . \
+  --ignore "target/**" \
+  --exts rs,sh,toml \
+  -r \
+  -- cargo run -p clip_srv
