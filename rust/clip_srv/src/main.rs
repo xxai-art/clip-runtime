06de77d0bd66f91e0495c60435044ca182521554
xxai.art <xxai.art@gmail.com>
2023-06-06 14:15:50 +0800

    ♨

--- rust/clip_srv/src/main.rs
+++ rust/clip_srv/src/main.rs
@@ -1,12 +1,22 @@
 use anyhow::Result;
+use qdrant_client::prelude::QdrantClientConfig;
 use std::env::var;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  let model_dir = var("MODEL_DIR")?;
   let grpc = var("QDRANT_GRPC")?;
-  // let config = QdrantClientConfig::from_url("http://localhost:");
+  let mut config = QdrantClientConfig::from_url(&grpc);
 
-  dbg!(model_dir, grpc);
+  if let Ok(key) = var("QDRANT__SERVICE__API_KEY") {
+    config.set_api_key(&key);
+  }
+  let client = QdrantClient::new(Some(config))?;
+
+  let li = client.list_collections().await?;
+
+  dbg!(li);
+  // let config = QdrantClientConfig::from_url("http://localhost:");
+  // let model_dir = var("MODEL_DIR")?;
+
   Ok(())
 }
