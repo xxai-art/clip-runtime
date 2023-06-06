accacaa0f557cbfe2d469b727e41478ad58d1a6f
xxai.art <xxai.art@gmail.com>
2023-06-06 17:12:00 +0800

    ♨

--- rust/clip_img2qdrant/src/main.rs
+++ rust/clip_img2qdrant/src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::OnceLock;
 
 use anyhow::Result;
+use awp::srv;
 use clip_qdrant::qdrant_client;
 use clip_runtime::{
   img::{clip_img, ClipImg},
@@ -12,7 +13,7 @@ static ONNX: OnceLock<ClipImg<clip_img::CropTop>> = OnceLock::new();
 pub fn clip_onnx() -> &'static ClipImg<clip_img::CropTop> {
   ONNX.get_or_init(|| {
     let ort = ClipOrt::new().unwrap();
-    let model = ort.model(std::env::var("ONNX_DIR").unwrap());
+    let model = ort.model(std::env::var("MODEL_DIR").unwrap());
     model.img("onnx/ImgNorm", 224, clip_img::CropTop()).unwrap()
   })
 }
