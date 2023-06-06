67b5dc9d30f5c9ac1bcbc0fa57b746b9970516e4
xxai.art <xxai.art@gmail.com>
2023-06-06 17:01:44 +0800

    ♨

--- rust/clip_img2qdrant/src/main.rs
+++ rust/clip_img2qdrant/src/main.rs
@@ -1,9 +1,26 @@
+use std::sync::OnceLock;
+
 use anyhow::Result;
 use clip_qdrant::qdrant_client;
+use clip_runtime::{
+  img::clip_img,
+  ort::{ClipModel, ClipOrt},
+};
+
+static MODEL: OnceLock<ClipModel> = OnceLock::new();
+
+pub fn clip_model() -> &'static ClipModel {
+  MODEL.get_or_init(|| {
+    let ort = ClipOrt::new().unwrap();
+    ort.model(std::env::var("MODEL_DIR").unwrap())
+  })
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
   let client = qdrant_client().await?;
+  let model = clip_model();
+  let clip_img = model.img("onnx/ImgNorm", 224, clip_img::CropCenter())?;
 
   println!("Hello, world!");
   Ok(())
