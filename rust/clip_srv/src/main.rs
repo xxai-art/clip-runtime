4c6075b0c3179e82b8c0a235949139395f1b6ec2
xxai.art <xxai.art@gmail.com>
2023-06-06 14:09:21 +0800

    ♨

--- rust/clip_srv/src/main.rs
+++ rust/clip_srv/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-  println!("Hello, world!2");
+use anyhow::Result;
+use std::env::var;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let model_dir = var("MODEL_DIR")?;
+  let grpc_port = var("QDRANT__SERVICE__GRPC_PORT")?;
+  // let config = QdrantClientConfig::from_url("http://localhost:");
+
+  dbg!(model_dir, grpc_port);
+  Ok(())
 }
