453e22c515beb28489f8542bd0d3ef97408ebc22
xxai.art <xxai.art@gmail.com>
2023-06-06 14:57:17 +0800

    ♨

--- rust/clip_srv/src/main.rs
+++ rust/clip_srv/src/main.rs
@@ -1,4 +1,4 @@
-use std::env::var;
+use std::{collections::BTreeSet, env::var};
 
 use anyhow::Result;
 use qdrant_client::{
@@ -19,34 +19,41 @@ async fn main() -> Result<()> {
   }
   let client = QdrantClient::new(Some(config))?;
 
+  let li = client
+    .list_collections()
+    .await?
+    .collections
+    .into_iter()
+    .map(|i| i.name)
+    .collect::<BTreeSet<_>>();
+
   let name = var("CLIP_NAME").unwrap_or_else(|_| "clip".into());
-  let li = client.list_collections().await?.collections;
-  dbg!(li);
+  if li.get(&name).is_none() {
+    client
+      .create_collection(&CreateCollection {
+        collection_name: name,
+        vectors_config: Some(VectorsConfig {
+          config: Some(Config::Params(VectorParams {
+            size: 1024,
+            distance: Distance::Euclid.into(),
+            on_disk: Some(true),
+            hnsw_config: None,
+            quantization_config: Some(QuantizationConfig {
+              quantization: Quantization::Scalar(qdrant_client::qdrant::ScalarQuantization {
+                r#type: qdrant_client::qdrant::QuantizationType::Int8 as i32,
+                quantile: Some(0.99),
+                always_ram: Some(false),
+              })
+              .into(),
+            }),
+          })),
+        }),
+        ..Default::default()
+      })
+      .await?;
+  }
 
   // let config = QdrantClientConfig::from_url("http://localhost:");
   // let model_dir = var("MODEL_DIR")?;
-  client
-    .create_collection(&CreateCollection {
-      collection_name: name,
-      vectors_config: Some(VectorsConfig {
-        config: Some(Config::Params(VectorParams {
-          size: 1024,
-          distance: Distance::Euclid.into(),
-          on_disk: Some(true),
-          hnsw_config: None,
-          quantization_config: Some(QuantizationConfig {
-            quantization: Quantization::Scalar(qdrant_client::qdrant::ScalarQuantization {
-              r#type: qdrant_client::qdrant::QuantizationType::Int8 as i32,
-              quantile: Some(0.99),
-              always_ram: Some(false),
-            })
-            .into(),
-          }),
-        })),
-      }),
-      ..Default::default()
-    })
-    .await?;
-
   Ok(())
 }
