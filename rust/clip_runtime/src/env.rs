76328a3d8f6fda8512147dc7a4fb50a5ac2fe4fc
xxai.art <xxai.art@gmail.com>
2023-06-06 15:33:21 +0800

    ♨

---
+++ rust/clip_runtime/src/env.rs
@@ -0,0 +1,8 @@
+pub static mut TO: String = String::new();
+
+pub fn env_default<T: std::str::FromStr>(env_var: &str, default: T) -> T {
+  match std::env::var(env_var) {
+    Ok(port) => port.parse().unwrap_or(default),
+    Err(_) => default,
+  }
+}
