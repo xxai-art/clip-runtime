6755ab95300d5639534d405a071aa5a6e7006ca0
xxai.art <xxai.art@gmail.com>
2023-06-06 16:49:49 +0800

    ♨

--- rust/clip_srv/src/env.rs
+++
@@ -1,6 +0,0 @@
-pub fn env_default<T: std::str::FromStr>(env_var: &str, default: T) -> T {
-  match std::env::var(env_var) {
-    Ok(port) => port.parse().unwrap_or(default),
-    Err(_) => default,
-  }
-}
