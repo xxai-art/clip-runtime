6ff884105e82ad4f8c5597ff6cbedb1fb6d5593e
xxai.art <xxai.art@gmail.com>
2023-06-09 21:12:32 +0800

    ♨

--- clip_pipe/lib/index.js
+++ clip_pipe/lib/index.js
@@ -1,16 +1,42 @@
 #!/usr/bin/env -S node --loader=@w5/jsext --trace-uncaught --expose-gc --unhandled-rejections=strict --experimental-import-meta-resolve
-var URL, bin, hash;
+var DB, URL, bin, hash;
 
 import reqBin from '@w5/req/reqBin';
 
+import {
+  Db
+} from '@w5/clip_img2qdrant';
+
 URL = 'https://5ok.pw/h950/';
+
+({
+  clip: DB
+} = Db);
 
 hash = '824lDpfyaIFkmQd0D9Qldg';
 
 bin = (await reqBin(URL + hash));
 
-1;
+// > ava:test
+//   ../index.js > Db
+//   @w5/uridir
+//   path > join dirname
+//   fs > readFileSync
 
-JSON.stringify({
-  t: [1, 2, 3]
-});
+// ROOT = dirname dirname dirname uridir import.meta
+
+// DB = Db.clip
+
+// test "clip",(t)=>
+//   fp = join ROOT,'lib/img/cat.jpg'
+//   img = readFileSync fp
+//   console.log await DB.add(
+//     1
+//     {
+//       t:[1,2,3]
+//     }
+//     img
+//     "jpg"
+//   )
+//   t.pass()
+//   return
--- clip_pipe/src/index.coffee
+++ clip_pipe/src/index.coffee
@@ -1,14 +1,39 @@
 #!/usr/bin/env coffee
 
 > @w5/req/reqBin
+  @w5/clip_img2qdrant > Db
 
 URL = 'https://5ok.pw/h950/'
+
+{clip:DB} = Db
+
 hash = '824lDpfyaIFkmQd0D9Qldg'
 
 bin = await reqBin URL+hash
 
-1
 
-JSON.stringify {
-  t:[1,2,3]
-}
+
+# > ava:test
+#   ../index.js > Db
+#   @w5/uridir
+#   path > join dirname
+#   fs > readFileSync
+#
+# ROOT = dirname dirname dirname uridir import.meta
+#
+# DB = Db.clip
+#
+# test "clip",(t)=>
+#   fp = join ROOT,'lib/img/cat.jpg'
+#   img = readFileSync fp
+#   console.log await DB.add(
+#     1
+#     {
+#       t:[1,2,3]
+#     }
+#     img
+#     "jpg"
+#   )
+#   t.pass()
+#   return
+
