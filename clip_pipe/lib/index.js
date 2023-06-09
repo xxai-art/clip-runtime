2549bee4f4775039e2d19eafa01f11fe714d1bd1
xxai.art <xxai.art@gmail.com>
2023-06-09 22:27:31 +0800

    ♨

--- clip_pipe/lib/index.js
+++ clip_pipe/lib/index.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env -S node --loader=@w5/jsext --trace-uncaught --expose-gc --unhandled-rejections=strict --experimental-import-meta-resolve
-var PORN, URL;
-
-import reqBin from '@w5/req/reqBin';
+var DB, PORN, URL;
 
 import {
   Db
 } from '@w5/clip_img2qdrant';
 
-import KV from '@w5/redis/KV';
+import gid from '@w5/gid';
 
 import {
   UPSERT
 } from '@w5/pg/PG';
 
+import KV from '@w5/redis/KV';
+
 import R from '@w5/redis/R';
 
-import gid from '@w5/gid';
+import redistream from '@w5/redistream';
+
+import reqBin from '@w5/req/reqBin';
 
 PORN = (await UPSERT('tag.tag', {
   val: 'porn'
@@ -23,19 +25,22 @@ PORN = (await UPSERT('tag.tag', {
 
 URL = 'https://5ok.pw/h950/';
 
+({
+  clip: DB
+} = Db);
 
-// {clip:DB} = Db
+await redistream.clip(async(id, hash, adult) => {
+  var bin, t, url;
+  hash = Buffer.from(hash).toString('base64url');
+  url = URL + hash;
+  console.log(url);
+  bin = (await reqBin(url));
+  t = [];
+  if (adult) {
+    t.push(PORN);
+  }
+  await DB.add(id, {t}, bin, 'avif');
+  return true;
+});
 
-// hash = '824lDpfyaIFkmQd0D9Qldg'
-
-// bin = await reqBin URL+hash
-
-// await DB.add(
-//   1
-//   {
-//     t: [1,3]
-//   }
-//   bin
-//   'avif'
-// )
 process.exit();
--- clip_pipe/src/index.coffee
+++ clip_pipe/src/index.coffee
@@ -1,29 +1,38 @@
 #!/usr/bin/env coffee
 
-> @w5/req/reqBin
-  @w5/clip_img2qdrant > Db
-  @w5/redis/KV
-  @w5/pg/PG > UPSERT
-  @w5/redis/R
+> @w5/clip_img2qdrant > Db
   @w5/gid
+  @w5/pg/PG > UPSERT
+  @w5/redis/KV
+  @w5/redis/R
+  @w5/redistream
+  @w5/req/reqBin
 
 PORN = await UPSERT('tag.tag',{val:'porn'})
 
 URL = 'https://5ok.pw/h950/'
-#
-# {clip:DB} = Db
-#
-# hash = '824lDpfyaIFkmQd0D9Qldg'
-#
-# bin = await reqBin URL+hash
-#
-# await DB.add(
-#   1
-#   {
-#     t: [1,3]
-#   }
-#   bin
-#   'avif'
-# )
+{clip:DB} = Db
+
+await redistream.clip (id, hash, adult)=>
+  hash = Buffer.from(hash).toString 'base64url'
+  url = URL+hash
+  console.log url
+  bin = await reqBin url
+
+  t = []
+  if adult
+    t.push PORN
+
+  await DB.add(
+    id
+    {
+      t
+    }
+    bin
+    'avif'
+  )
+
+  return true
+
 
 process.exit()
