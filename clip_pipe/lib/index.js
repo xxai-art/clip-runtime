768be6c570a543919a24066dc3c8a5dfe3b2cd3c
xxai.art <xxai.art@gmail.com>
2023-06-09 21:15:37 +0800

    ♨

--- clip_pipe/lib/index.js
+++ clip_pipe/lib/index.js
@@ -1,46 +1,24 @@
 #!/usr/bin/env -S node --loader=@w5/jsext --trace-uncaught --expose-gc --unhandled-rejections=strict --experimental-import-meta-resolve
-var DB, URL, bin, hash;
-
 import reqBin from '@w5/req/reqBin';
 
 import {
   Db
 } from '@w5/clip_img2qdrant';
 
-URL = 'https://5ok.pw/h950/';
+// URL = 'https://5ok.pw/h950/'
 
-({
-  clip: DB
-} = Db);
+// {clip:DB} = Db
 
-hash = '824lDpfyaIFkmQd0D9Qldg';
+// hash = '824lDpfyaIFkmQd0D9Qldg'
 
-bin = (await reqBin(URL + hash));
+// bin = await reqBin URL+hash
 
-await DB.add(1, {
-  t: [1, 3]
-}, bin, 'avif');
+// await DB.add(
+//   1
+//   {
+//     t: [1,3]
+//   }
+//   bin
+//   'avif'
+// )
 
-// > ava:test
-//   ../index.js > Db
-//   @w5/uridir
-//   path > join dirname
-//   fs > readFileSync
-
-// ROOT = dirname dirname dirname uridir import.meta
-
-// DB = Db.clip
-
-// test "clip",(t)=>
-//   fp = join ROOT,'lib/img/cat.jpg'
-//   img = readFileSync fp
-//   console.log await DB.add(
-//     1
-//     {
-//       t:[1,2,3]
-//     }
-//     img
-//     "jpg"
-//   )
-//   t.pass()
-//   return
--- clip_pipe/src/index.coffee
+++ clip_pipe/src/index.coffee
@@ -3,44 +3,20 @@
 > @w5/req/reqBin
   @w5/clip_img2qdrant > Db
 
-URL = 'https://5ok.pw/h950/'
-
-{clip:DB} = Db
-
-hash = '824lDpfyaIFkmQd0D9Qldg'
-
-bin = await reqBin URL+hash
-
-await DB.add(
-  1
-  {
-    t: [1,3]
-  }
-  bin
-  'avif'
-)
-
-# > ava:test
-#   ../index.js > Db
-#   @w5/uridir
-#   path > join dirname
-#   fs > readFileSync
+# URL = 'https://5ok.pw/h950/'
 #
-# ROOT = dirname dirname dirname uridir import.meta
+# {clip:DB} = Db
 #
-# DB = Db.clip
+# hash = '824lDpfyaIFkmQd0D9Qldg'
 #
-# test "clip",(t)=>
-#   fp = join ROOT,'lib/img/cat.jpg'
-#   img = readFileSync fp
-#   console.log await DB.add(
-#     1
-#     {
-#       t:[1,2,3]
-#     }
-#     img
-#     "jpg"
-#   )
-#   t.pass()
-#   return
-
+# bin = await reqBin URL+hash
+#
+# await DB.add(
+#   1
+#   {
+#     t: [1,3]
+#   }
+#   bin
+#   'avif'
+# )
+#
