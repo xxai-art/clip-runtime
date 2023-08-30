fe281d891ed427b81d2d2c375bd30d029cee6a08
xxai.art <xxai.art@gmail.com>
2023-08-30 18:30:12 +0800

    ♨

--- clip_pipe/clip.sh
+++ clip_pipe/clip.sh
@@ -8,5 +8,5 @@ if [ -f "/etc/profile" ]; then
   source /etc/profile
 fi
 
-nc -z -w 1 127.0.0.1 7890 && export http_proxy=http://127.0.0.1:7890 https_proxy=$http_proxy
+nc -z -w 1 127.0.0.1 7890 && export https_proxy=http://127.0.0.1:7890
 exec ./lib/index.js
