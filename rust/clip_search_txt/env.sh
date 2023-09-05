bb667a2a6e85e03796d8ddcf3c3d07ccda2a74e8
xxai.art <xxai.art@gmail.com>
2023-09-05 23:14:52 +0800

    ♨

--- rust/clip_search_txt/env.sh
+++ rust/clip_search_txt/env.sh
@@ -9,3 +9,28 @@ ROOT=$(realpath $DIR/../..)
 export ORT_LIB_LOCATION=$ROOT/lib/so
 export ORT_DYLIB_PATH=$ORT_LIB_LOCATION/onnxruntime.dll
 export MODEL_DIR=$ROOT/lib/model/AltCLIP-XLMR-L-m18
+
+env_sh() {
+  local dir_conn=$(dirname $ROOT)/conf/conn
+  if [ ! -d "$dir_conn" ]; then
+    echo "$dir_conn not exist"
+    return
+  fi
+
+  cd $dir_conn
+  local i
+  for i in $@; do
+    if [ -f "$i.sh" ]; then
+      set -o allexport
+      source "$i".sh
+      set +o allexport
+    else
+      echo "$i not exist"
+    fi
+  done
+
+  cd $DIR
+  unset -f env_sh
+}
+
+env_sh host qdrant
