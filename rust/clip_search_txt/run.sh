1f640a7376ec283e5ea36b28ec445e8fe668f3c6
xxai.art <xxai.art@gmail.com>
2023-09-05 22:53:25 +0800

    ♨

--- rust/clip_search_txt/run.sh
+++ rust/clip_search_txt/run.sh
@@ -4,15 +4,11 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 export RUST_LOG="debug,globset=warn,watchexec=off,supervisor=warn,hyper=warn,rustls=warn,h2=warn,tower=warn,ort=warn"
-export MODEL=AltCLIP-XLMR-L-m18
-
-export MODEL_URL=https://huggingface.co/xxai-art/tar/resolve/main/$MODEL.tar.zstd
 
 ROOT=$(realpath $DIR/../..)
 
 export ORT_LIB_LOCATION=$ROOT/lib/so
 export ORT_DYLIB_PATH=$ORT_LIB_LOCATION/onnxruntime.dll
-
-export MODEL_DIR=$ROOT/lib/model/$MODEL
+export MODEL_DIR=$ROOT/lib/model/AltCLIP-XLMR-L-m18
 
 exec cargo run
